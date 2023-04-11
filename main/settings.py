a177df9a2a6b1d00f762bd6dee6e966ed3f21bbb
maxcrimea <maxcrimea50@gmail.com>
2023-04-11 21:37:44 +0600

    hw_8

--- main/settings.py
+++ main/settings.py
@@ -2,20 +2,14 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -28,6 +22,7 @@ INSTALLED_APPS = [
     'CAR',
     'parser_app',
     'custom_users',
+    'cloth',
 ]
 
 MIDDLEWARE = [
